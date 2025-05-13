399367b42be0149ba07d18c3048bfc11e2a9224a
geethatadi <gtadi@uncc.edu>
2025-05-13 13:18:57 -0400

    Create ProfileViewModel.swift

---
+++ ProfileViewModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class ProfileViewModel {
+
+    func askMeFeature() {
+        // Handle logic for turning on "Ask Me" feature
+        print("Ask Me feature enabled.")
+    }
+    
+    func addChannels() {
+        // Handle logic for adding channels
+        print("Channels feature opened.")
+    }
+
+    func performAction() {
+        // Handle "+" button action, such as opening a modal or a new screen
+        print("Performing action, possibly opening a new view.")
+    }
+}
