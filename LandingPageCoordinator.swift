95a42919f242fcfb200de661e3fcabb180109f76
geethatadi <gtadi@uncc.edu>
2025-06-01 00:45:23 -0400

    Update LandingPageCoordinator.swift

--- LandingPageCoordinator.swift
+++ LandingPageCoordinator.swift
@@ -9,4 +9,12 @@ class LandingPageCoordinator {
             topViewController.navigationController?.pushViewController(loginVC, animated: true)
         }
     }
+    
+    func goToFeedScreen() {
+        let feedVC = FeedViewController()  // Initialize FeedViewController
+        
+        if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
+            topViewController.navigationController?.pushViewController(feedVC, animated: true)
+        }
+    }
 }
